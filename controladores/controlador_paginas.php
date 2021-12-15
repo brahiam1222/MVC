00925e27fa7ceeb79fca42ac44df1510e235d05e
brahi <brahi@DESKTOP-8V1T4Q4>
2021-12-15 11:54:23 -0500

    iniciando2

---
+++ controladores/controlador_paginas.php
@@ -0,0 +1,22 @@
+<?php    
+
+    class ControladorPaginas
+    {
+            public function Inicio()
+            {
+                include_once("vistas/paginas/inicio.php")
+
+
+            }
+    {
+            public function Error()
+            {
+                include_once("vistas/paginas/error.php")
+                
+
+            }
+
+
+    }
+
+?>
